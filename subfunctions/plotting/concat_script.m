29bc03a92c4d37e80b163f9a9063985571bb3315
suliu1129 <40217182+suliu1129@users.noreply.github.com>
2019-04-26 16:27:54 -0700

    Add files via upload

--- subfunctions/plotting/concat_script.m
+++ subfunctions/plotting/concat_script.m
@@ -1,6 +1,11 @@
-dirs.data_root = '/Users/suliu/Dropbox/DATA/Pedro_data/data/neuralData';
-sbj_name = 'S18_127';
-project_name = 'Calculia_production';
-block_names = {'E18-706_0022', 'E18-706_0025', 'E18-706_0026'};
+% dirs.data_root = '/Users/suliu/Dropbox/DATA/Pedro_data/data/neuralData';
+% sbj_name = 'S18_127';
+% project_name = 'Calculia_production';
+% block_names = {'E18-706_0022', 'E18-706_0025', 'E18-706_0026'};
+
+dirs.data_root = '/Users/suliu/Desktop/136/neuralData';
+sbj_name = 'S19_136_AG';
+project_name = 'MMR';
+block_names = {'E19-320_0004'};
 plot_params.blc = true;
 data_all = ConcatenateAll(sbj_name,project_name,block_names,dirs,[],'HFB','stim', plot_params);
